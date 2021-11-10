bcbec614e71dab85402e1e1625520e566b0f6814
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2021-11-10 16:32:17 +0800

    added details to productpage

--- src/components/Pages/ProductPage.js
+++ src/components/Pages/ProductPage.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom'
-import Grid from '@mui/material/Grid';
-import Products from '../ProductCard';
+import { Box, Container, Card, CardContent, Typography, CardMedia } from '@mui/material';
 
 export default function ProductPage() {
-    const params = useParams(); 
+    const params = useParams();
     const [data, setData] = useState([]);
 
     const fetchData = async () => {
@@ -27,12 +26,33 @@ export default function ProductPage() {
     }, [params.category]);
 
     return (
-        <>
-        <div style={{minHeight: '62vh', maxWidth:1000, margin:'auto'}}>
-        <Grid sx={{justifyContent: 'center'}} container spacing={0.5} >
-            <Products data={data}/>
-        </Grid>
-        </div>
-        </>
+        <Container maxWidth='1000'>
+            <Card sx={{ display: 'flex', justifyContent:'center', padding:10, boxShadow:'none' }}>
+                <CardMedia
+                    component="img"
+                    sx={{ width: 300, padding: 5 }}
+                    image={data.image}
+                    alt={data.title}
+                />
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <CardContent>
+                        <Typography sx={{padding:1}} variant="h6" fontWeight='bold'>
+                            {data.title}
+                        </Typography>
+                        <Typography sx={{padding:1}} variant="subtitle1" >
+                            {data.rating?data.rating.rate:''}/5 ({data.rating?data.rating.count:''})
+                        </Typography>
+                        <Typography sx={{padding:1}} variant="subtitle1" color="text.secondary" >
+                            {data.description}
+                        </Typography>
+                        <Typography sx={{padding:1}} variant="subtitle1" color="text.secondary">
+                            US ${data.price}<br/>
+                            #Quantity
+                            #AddToCart
+                        </Typography>
+                    </CardContent>
+                </Box>
+            </Card>
+        </Container>
     );
 };
