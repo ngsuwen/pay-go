e17e99f7e00f0fd0e71a1dee724890e447732392
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2021-11-12 17:40:17 +0800

    reordered tabs

--- src/components/Pages/General/Tab.js
+++ src/components/Pages/General/Tab.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { styled, Tabs, Tab, Box } from '@mui/material';
-import {Link} from 'react-router-dom';
+import { styled, Tabs, Tab, Box, Container, IconButton, Badge } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PersonIcon from '@mui/icons-material/Person';
+import SearchIcon from '@mui/icons-material/Search';
+import { Link } from 'react-router-dom';
 
 const StyledTabs = styled((props) => (
     <Tabs
@@ -20,19 +23,19 @@ const StyledTabs = styled((props) => (
     }
 });
 
-const StyledTab = styled((props) => <Link style={{textDecoration:'none'}}to={`${props.link}`}><Tab disableRipple {...props} /></Link>)({
-        textTransform: 'none',
-        color: 'rgb(169,169,169)',
-        '&.Mui-selected': {
-            color: 'rgb(80,80,80)',
-        },
-        '&.Mui-focusVisible': {
-            backgroundColor: 'rgba(100, 95, 228, 0.32)',
-        },
+const StyledTab = styled((props) => <Link style={{ textDecoration: 'none' }} to={`${props.link}`}><Tab disableRipple {...props} /></Link>)({
+    textTransform: 'none',
+    color: 'rgb(169,169,169)',
+    '&.Mui-selected': {
+        color: 'rgb(80,80,80)',
     },
+    '&.Mui-focusVisible': {
+        backgroundColor: 'rgba(100, 95, 228, 0.32)',
+    },
+},
 );
 
-export default function CustomizedTabs() {
+export default function CustomizedTabs({ cart }) {
     const [value, setValue] = React.useState(0);
 
     const handleChange = (event, newValue) => {
@@ -40,21 +43,34 @@ export default function CustomizedTabs() {
     };
 
     return (
-        <Box sx={{ marginBottom: 5 }}>
-            <StyledTabs
-                value={value}
-                onChange={handleChange}
-                centered
-            >
-                <StyledTab label="HOME" link='/'/>
-                <StyledTab label="ELECTRONICS" link='/category/electronics' />
-                <StyledTab label="JEWELLERY" link='/category/jewelery'/>
-                <StyledTab label="MENS" link="/category/men's%20clothing"/>
-                <StyledTab label="WOMENS" link="/category/women's%20clothing"/>
-                <StyledTab label="SIGN IN" />
-                <StyledTab label="CART" link="/cart"/>
-                <StyledTab label="SEARCH" />
-            </StyledTabs>
-        </Box>
+        <Container maxWidth='lg'>
+            <Box sx={{ marginBottom: 5 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <IconButton>
+                        <SearchIcon />
+                    </IconButton>
+                    <IconButton>
+                        <PersonIcon />
+                    </IconButton>
+                    <Link to="/cart">
+                        <IconButton>
+                            <Badge badgeContent={cart.length} color="default">
+                                <ShoppingCartIcon />
+                            </Badge>
+                        </IconButton>
+                    </Link>
+                </Box>
+                <StyledTabs
+                    value={value}
+                    onChange={handleChange}
+                >
+                    <StyledTab label="HOME" link='/' />
+                    <StyledTab label="ELECTRONICS" link='/category/electronics' />
+                    <StyledTab label="JEWELLERY" link='/category/jewelery' />
+                    <StyledTab label="MENS" link="/category/men's%20clothing" />
+                    <StyledTab label="WOMENS" link="/category/women's%20clothing" />
+                </StyledTabs>
+            </Box>
+        </Container>
     );
 }
