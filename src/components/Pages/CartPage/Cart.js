0b09f484b45b82ed75c2c6c6b91a13b997d14ec4
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2021-11-17 16:14:06 +0800

    checkout button opens dialog and clears cart

--- src/components/Pages/CartPage/Cart.js
+++ src/components/Pages/CartPage/Cart.js
@@ -1,19 +1,30 @@
-import React, { useContext } from 'react';
-import { List, Container, ListItem, Typography, Button, Grid, Divider } from '@mui/material';
+import React, { useContext, useState } from 'react';
+import { List, Container, ListItem, Typography, Button, Grid, Divider, Dialog, DialogContent, DialogContentText, DialogActions } from '@mui/material';
 import CartItem from './CartItem';
-import { DataContext, CurrencyContext , RateContext } from '../../../App'
+import { DataContext, CurrencyContext, RateContext } from '../../../App'
 
 
 export default function Cart() {
+    const [open, setOpen] = useState(false)
     const [cart, setCart] = useContext(DataContext);
     const [rate, setRate] = useContext(RateContext);
     const [currency, setCurrency] = useContext(CurrencyContext);
 
     const cartItemList = cart.map((item, index) =>
         <CartItem data={item} key={index} />)
-    let totalCost=0
-    const calTotalCost=()=> cart.forEach(item => totalCost+=item.quantity*item.price*(rate?rate:1))
+    let totalCost = 0
+    const calTotalCost = () => cart.forEach(item => totalCost += item.quantity * item.price * (rate ? rate : 1))
     calTotalCost()
+
+    const openHandler = () => {
+        setOpen(true)
+        setCart([])
+    }
+
+    const closeHandler = () => {
+        setOpen(false)
+    }
+
     return (
         <Container maxWidth='lg' sx={{ minHeight: '62vh' }}>
             <List
@@ -31,15 +42,30 @@ export default function Cart() {
                     <Grid container direction="column" spacing={3} >
                         <Grid item />
                         <Grid item style={{ display: "flex", justifyContent: "flex-end" }}>
-                            <Typography fontWeight='bold' color="text.secondary">Total Cost: {currency?currency.toUpperCase():'USD $'}{Number(totalCost).toFixed(2)}</Typography>
+                            <Typography fontWeight='bold' color="text.secondary">Total Cost: {currency ? currency.toUpperCase() : 'USD $'}{Number(totalCost).toFixed(2)}</Typography>
                         </Grid><Grid item style={{ display: "flex", justifyContent: "flex-end" }}>
-                            <Button sx={{ color: "text.secondary", fontSize: 12 }} variant="outlined" color='inherit'>Check out</Button>
+                            <Button sx={{ color: "text.secondary", fontSize: 12 }} variant="outlined" color='inherit' onClick={openHandler}>Check out</Button>
                         </Grid>
                         <Grid item />
                     </Grid>
                 </ListItem>
-
             </List>
+            <Dialog
+                open={open}
+                onClose={closeHandler}
+                aria-labelledby="responsive-dialog-title"
+            >
+                <DialogContent>
+                    <DialogContentText>
+                        Thank you for shopping with us!
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus onClick={closeHandler} sx={{ color: "text.secondary", fontSize: 12 }}>
+                        OK
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Container>
     )
 }
